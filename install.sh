a4eb3b207dbb39cecc03ebc7201d77e2b835f53f
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-01-11 13:03:43 +0200

    Added ssmtp.conf for inbox.lv and revaliases for root user, install.sh started

---
+++ install.sh
@@ -0,0 +1,4 @@
+echo "Please make sure these are installed: systemd, logkeys, scrot, gpg"
+echo "Logkeys can be found here: http://code.google.com/p/logkeys/source/checkout"
+echo -n "Press enter to continue..."
+read
