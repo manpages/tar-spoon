7e466cc8961b853b6073e567e35f30127abe9d19
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-01-11 13:55:50 +0200

    Added systemd service and reverse tunnel installer

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-echo "Please make sure these are installed: systemd, logkeys, scrot, gpg"
+# todo: archlinux PKGBUILD for tar-spoon
+echo "Please make sure these are installed: sudo, systemd, logkeys, scrot, wicd, gpg"
 echo "Logkeys can be found here: http://code.google.com/p/logkeys/source/checkout"
 echo -n "Press enter to continue..."
 read
+
