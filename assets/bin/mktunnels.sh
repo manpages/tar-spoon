80ccc09d1912d5524217c4064fdfc00bfc9a8629
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-01-12 04:07:28 +0200

    Reverse tunnel generator works

--- assets/bin/mktunnels.sh
+++ assets/bin/mktunnels.sh
@@ -4,13 +4,44 @@ function errcho {
 }
 
 function mktun {
+	local prefix="-f"
+	local suffix=""
+	local key_path=""
+	local kitty=""
+	local m_port=$[ ( $RANDOM % 10000 )  + 10000 ]
 	local tun=$1
+
 	errcho "Make a verbose log about this tunnel? [y/N] "
 	read answer
 	if [ "$answer" == 'y' ] ; then
-		errcho "Yeah, fuck off"
+		prefix="-vv"
+		suffix="> /var/log/tarspoon/$tun-out.log"
 	fi
+	errcho "Path to the laptop --> middleman key. [~/.ssh/id_rsa]"
+	read key_path
+	[[ -z $key_path ]] && key_path="~/.ssh/id_rsa"
+
+	errcho "Middleman server hostname or ip address:"
+	read kitty
+	[[ -z $kitty ]] && return
+
+	errcho "Middleman username: [$SUDO_USER]"
+	read middleman
+	[[ -z $middleman ]] && middleman="$SUDO_USER"
+
+	errcho "Let's try to copy the public key over..."
+	su -c "ssh-copy-id -i $key_path $middleman@$kitty" root >/dev/null 2>/dev/null
+	
+	echo "#!/bin/bash
+	export AUTOSSH_PORT=0
+	export AUTOSSH_GATETIME=0
+	autossh $prefix -- -o 'ControlPath none' -i $key_path -R $m_port:localhost:22 $middleman@$kitty -N $suffix 2>/var/log/tarspoon/$tun-err.log
+	" > /usr/local/bin/tun_$tun
+
+	chmod +x /usr/local/bin/tun_$tun
 }
+
+mkdir -p /var/log/tarspoon
 
 errcho "You're about to setup reverse SSH tunnels that will be used to penetrate NAT."
 errcho "  I tend to setup two tunnels — local (for testing purposes) and real-world."
