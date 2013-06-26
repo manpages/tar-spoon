d64ffd659fd2807a200625ec524577b0ca005691
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-06-26 06:03:27 +0300

    Don't use inbox.lv

--- install.sh
+++ install.sh
@@ -6,27 +6,20 @@ echo "Logkeys can be found here: http://code.google.com/p/logkeys/source/checkou
 echo -n "Press enter to continue..."
 read
 
-echo "Enter dummy http://inbox.lv account name and password."
-echo "  Those will be used to forward encrypted mails with laptop activity logs to your actual email"
-echo "  If you don't have inbox.lv account, create one now."
-echo "  It's awesome for throw-away mailboxes, as it doesn't expire and tolerates registration over Tor."
-echo "  Don't forget to allow external SMTP access here:"
-echo "  http://mail.inbox.lv/horde/imp/prefs.php?language=en&group=forward"
+echo "Enter file server to put encrypted collected data on over SSH."
+echo "Consider restricting user shell, chroot jail and other limitations on the user that is capable of shipping files"
+echo "Ideally, replace file over SSH approach with more appropriate (note that the files are encrypted anyway)!"
+echo "It is better to use IP here."
+read rhost
 
-echo -n "Inbox.lv username (empty to skip that step): "
-read INBOXLV_USERNAME
-if [[ ! -z $INBOXLV_USERNAME ]]; then
-	echo -n "Inbox.lv password: "
-	read INBOXLV_PASSWORD
+echo "Where is the key to authenticate at the remote?"
+read key
 
-	echo "Backing up existing ssmtp.conf and revaliases [invoking sudo]"
-	sudo mv /etc/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp.conf.old 2>/dev/null
-	sudo mv /etc/ssmtp/revaliases /etc/ssmtp/revaliases.old 2>/dev/null
+echo "What is the remote user name?"
+read ruser
 
-	echo "Putting new ssmtp.conf and revaliases to /etc/ssmtp/"
-	sed -r "s#t4Rp07#$INBOXLV_USERNAME#;s#p455w0rd#$INBOXLV_PASSWORD#" assets/conf/ssmtp.conf | sudo tee /etc/ssmtp/ssmtp.conf 1>/dev/null
-	sed -r "s#t4Rp07#$INBOXLV_USERNAME#" assets/conf/revaliases | sudo tee /etc/ssmtp/revaliases 1>/dev/null
-fi
+echo "#!/bin/bash
+      scp -ri ${key} /tmp/yaourt-tmp-root/scrot/package/* ${ruser}@${rhost}:/tmp/ && rm -f \"/tmp/yaourt-tmp-root/scrot/package/*\"" > /usr/local/bin/tsp_mv
 
 echo "Enter desired local ssh SOCKS proxy port (empty to skip that step)" 
 read SOCKS_PROXY_PORT
